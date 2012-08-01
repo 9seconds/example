4bab367098c17a8770ebaa835a6547d2b3d1cc2c
9seconds <nineseconds@yandex.ru>
2012-08-01 17:27:44 +0400

    SICPed

--- fib.c
+++ fib.c
@@ -2,11 +2,31 @@
 #include <stdlib.h>
 
 
+
+#define EVEN(number) ( !(number % 2) )
+
+
+
+static int
+calculate_fibonacci (int a, int b, int p, int q, int count);
+
+
 int
 fibonacci (int number) {
     return ( number < 2 )
         ? number
-        : fibonacci(number-1) + fibonacci(number-2);
+        : calculate_fibonacci(1, 0, 0, 1, number);
+}
+
+
+static int
+calculate_fibonacci (int a, int b, int p, int q, int count) {
+    if ( !count )
+        return b;
+    else
+        return ( EVEN(count) )
+            ? calculate_fibonacci(a, b, p*p+q*q, q*(2*p+q), count/2)
+            : calculate_fibonacci(q*(b+a)+a*p, b*p+a*q, p, q, count-1);
 }
 
 
