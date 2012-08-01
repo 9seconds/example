6b092d7b34126d169215075f71f769d6c073391f
9seconds <nineseconds@yandex.ru>
2012-08-01 16:37:03 +0400

    Added argument verification

--- fib.c
+++ fib.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int
 main (int argc, char *argv[]) {
-    printf("Hello, world!");
+    if ( argc != 2 ) {
+        printf("Usage: fib <integer positive number>");
+        return 1;
+    }
+
+    printf("0");
+
     return 0;
 }
