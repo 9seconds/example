a4c58a235129a327191e6148042aa61b070dad91
9seconds <nineseconds@yandex.ru>
2012-08-01 16:40:08 +0400

    Added first version of function

--- fib.c
+++ fib.c
@@ -3,6 +3,19 @@
 
 
 int
+fibonacci (number) {
+    if ( number == 0 ) {
+        return 0;
+    }
+    if ( number == 1 ) {
+        return 1;
+    }
+
+    return fibonacci(number-1) + fibonacci(number-2);
+}
+
+
+int
 main (int argc, char *argv[]) {
     if ( argc != 2 ) {
         printf("Usage: fib <integer positive number>");
